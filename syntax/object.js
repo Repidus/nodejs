ef484308485c7bb752dda4d51106aaf060a595fa
REPIDUS <moondj123@naver.com>
2019-05-14 23:20:55 +0900

    Practice JavaScript object
    실습: JavaScript의 객체 문법을 실습함.

---
+++ syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['Albert', 'Bethany', 'Charles'];
+console.log(members[1]);  // Bethany
+
+var roles = {
+  'programmer': 'Albert', 
+  'designer': 'Bethany',
+  'manager': 'Charles'
+}
+console.log(roles.designer);  // Bethany
