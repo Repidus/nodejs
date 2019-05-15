ede06660fadbfae87225fcec0ada5e3f6fcac5da
REPIDUS <moondj123@naver.com>
2019-05-15 20:23:06 +0900

    Practice handling JavaScript object with loop
    실습: JavaScript 객체를 반복문을 이용해서 다루는 문법을 실습함.

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,10 @@
 var members = ['Albert', 'Bethany', 'Charles'];
 console.log(members[1]);  // Bethany
+var i = 0;
+while (i < members.length) {
+  console.log('array loop', members[i]);
+  i += 1;
+}
 
 var roles = {
   'programmer': 'Albert', 
@@ -7,3 +12,8 @@ var roles = {
   'manager': 'Charles'
 }
 console.log(roles.designer);  // Bethany
+console.log(roles['designer']); // Bethany
+
+for (var name in roles) {
+  console.log('object =>', name, 'value =>', roles[name]);
+}
