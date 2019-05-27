5cf24cec7f4d7c436cea9a88931fa32ee2388e05
REPIDUS <moondj123@naver.com>
2019-05-27 16:21:23 +0900

    Get template object out of main.js, and make it a module
    리팩토링: template 객체를 모듈화하여 lib 디렉터리로 분리함.

--- main.js
+++ main.js
@@ -2,34 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-var template = {
-  HTML: function(title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list: function(fileList) {
-    var list = '<ul>';
-    fileList.forEach(filename => {
-      list += `<li><a href="/?id=${filename}">${filename}</a></li>`;
-    });
-    list += '</ul>';
-    return list;
-  }
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response) {
 	var _url = request.url;
